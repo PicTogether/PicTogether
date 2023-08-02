210063107c770675b2428b2ed151c963bd1f4a77
Jiyeon Na <2021038122@hanyang.ac.kr>
2023-08-02 16:51:21 +0900

    fix: modify uploadTime

--- pic_together/lib/model/photo.dart
+++ pic_together/lib/model/photo.dart
@@ -6,6 +6,6 @@ class Photo {
   Photo(String id, String uploaderId, String uploadTime) {
     this.id = id;
     this.uploaderId = uploaderId;
-    this.uploadTime = DateTime(2023, 7, 31, 0, 0, 0);
+    this.uploadTime = uploadTime
   }
 }
