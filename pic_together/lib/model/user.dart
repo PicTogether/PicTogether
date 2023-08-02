dbf87b4a7ae1d460ba3c9b8ff2f662426dea1b9d
bjsbest <bjsbest@hanyang.ac.kr>
2023-08-02 14:57:48 +0900

    fix : User 변수 appointmentId 추가

--- pic_together/lib/model/user.dart
+++ pic_together/lib/model/user.dart
@@ -3,11 +3,13 @@ class User {
   late String name;
   late String phoneNum;
   late List<String> friends;
+  late String appointmentId;
 
-  User(String userId, String name, String phoneNum, List<String> friends) {
+  User(String userId, String name, String phoneNum, List<String> friends, String appointmentId) {
     this.userId = userId;
     this.name = name;
     this.phoneNum = phoneNum;
     this.friends = friends;
+    this.appointmentId = appointmentId;
   }
 }
