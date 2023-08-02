1c347867162145b102cf03b4eacea6af9bd4a7b4
bjsbest <bjsbest@hanyang.ac.kr>
2023-08-02 20:05:49 +0900

    fix : 돌림

--- pic_together/lib/view/main_view/main_view.dart
+++ pic_together/lib/view/main_view/main_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_flutter_aj/calender_flutter.dart';
-import 'package:pic_together/controller/firebase_controller.dart';
-import 'package:pic_together/model/appointment.dart';
+import 'package:pic_together/view/add_friend_view/add_friend_view.dart';
+import 'package:pic_together/view/gallery_view/gallery_view.dart';
+import 'package:pic_together/view/appointment_view/add_appointment_view.dart';
+
+import '../../controller/firebase_controller.dart';
+import '../../model/appointment.dart';
 
 void main() {
   runApp(const MainView());
@@ -24,24 +28,19 @@ class _MainViewState extends State<MainView> {
       title: 'Calender Flutter AJ',
       home: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.white,
-            title: const Text(
-              'PicTogether',
-              style: TextStyle(color: Colors.black),
-            ),
-            leading: GestureDetector(
-              onTap: () {
-                _showAppointmentPopup(context);
-              },
-              child: const SizedBox(
-                width: 50,
-                height: 50,
-                child: Icon(
-                  Icons.add,
-                  color: Colors.black,
-                ),
-              ),
-            )),
+          backgroundColor: Colors.white,
+          title: Text(
+            'PicTogether',
+            style: TextStyle(color: Colors.black),),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add,
+                color: Colors.black,),
+              onPressed: () {  },
+              tooltip: '약속 추가',
+            )
+          ],
+        ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
@@ -133,11 +132,14 @@ class _MainViewState extends State<MainView> {
           children: [
             FloatingActionButton(
               onPressed: () {
-                // 사진첩 들어가는는 작업 수행
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>GalleryView(),
+                    ),
+                );
               },
-              child: Icon(
-                Icons.photo_library,
-              ),
+              child: Icon(Icons.photo_library,),
               backgroundColor: Color(0xff655DBB),
               tooltip: '사진첩',
             ),
@@ -145,10 +147,14 @@ class _MainViewState extends State<MainView> {
             FloatingActionButton(
               onPressed: () {
                 // 홈 화면으로 이동하는 작업 수행
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>MainView(),
+                  ),
+                );
               },
-              child: Icon(
-                Icons.home,
-              ),
+              child: Icon(Icons.home,),
               backgroundColor: Color(0xff655DBB),
               tooltip: '홈 화면',
             ),
@@ -156,10 +162,14 @@ class _MainViewState extends State<MainView> {
             FloatingActionButton(
               onPressed: () {
                 // 친구 추가 화면으로 이동하는 작업 수행
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                    builder: (context) =>AddFriendView(),
+                    ),
+                );
               },
-              child: Icon(
-                Icons.person_add,
-              ),
+              child: Icon(Icons.person_add,),
               backgroundColor: Color(0xff655DBB),
               tooltip: '친구 추가',
             ),
@@ -168,7 +178,6 @@ class _MainViewState extends State<MainView> {
       ),
     );
   }
-
   void _showAppointmentPopup(BuildContext context) {
     String name = '';
     DateTime selectedDate = DateTime.now();
@@ -220,7 +229,7 @@ class _MainViewState extends State<MainView> {
                 }
 
                 Appointment appointment =
-                    Appointment.newAppointment(name, selectedDate);
+                Appointment.newAppointment(name, selectedDate);
 
                 FirebaseController().sendAppointmentData(appointment);
 
