b69a18c4d270b7155f7818626ce7faf2eeefd530
Jiyeon Na <2021038122@hanyang.ac.kr>
2023-08-02 16:25:35 +0900

    fix: friend_request, appointment model

--- pic_together/lib/model/appointment.dart
+++ pic_together/lib/model/appointment.dart
@@ -1,11 +1,13 @@
 class Appointment {
   late String id;
+  late String title;
   late DateTime date;
   late List<String> people;
   late String galleryId;
 
   Appointment(String id, DateTime date, List<String> people, String galleryId) {
     this.id = id;
+    this.title = title;
     this.date = date;
     this.people = people;
     this.galleryId = galleryId;
--- pic_together/lib/model/friend_request.dart
+++ pic_together/lib/model/friend_request.dart
@@ -1,9 +1,11 @@
 class FriendRequest {
+  late String id;
   late String requesterId;
   late String responderId;
   bool ok = false;
 
   FriendRequest(String requesterId, String responderId) {
+    this.id = id;
     this.requesterId = requesterId;
     this.responderId = responderId;
   }
