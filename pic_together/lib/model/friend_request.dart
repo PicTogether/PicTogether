739ebddf6f9c8af811f4ad5866be470f5500c584
Jiyeon Na <2021038122@hanyang.ac.kr>
2023-08-02 16:51:05 +0900

    fix: fix ok

--- pic_together/lib/model/friend_request.dart
+++ pic_together/lib/model/friend_request.dart
@@ -2,11 +2,12 @@ class FriendRequest {
   late String id;
   late String requesterId;
   late String responderId;
-  bool ok = false;
+  bool ok = false
 
-  FriendRequest(String requesterId, String responderId) {
+  FriendRequest(String id, String requesterId, String responderId, bool ok) {
     this.id = id;
     this.requesterId = requesterId;
     this.responderId = responderId;
+    this.ok = ok;
   }
 }
